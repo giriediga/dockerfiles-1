858d3e052852cfdc7ce6c921dfd92892b9fc8aa3
Giridhar <giri.ge@mymail.com>
2024-01-29 00:27:04 +0530

    Locking S3 bucket using DynamoDb locking Key

--- foreach/provider.tf
+++ foreach/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.31.0" # AWS provider version, not terraform version
     }
   }
+
+   backend "s3" {
+    bucket = "ardaimys3bucket" # this is your bucket name
+    key    = "foreach" # you can give any key ex, foreach
+    region = "us-east-1"
+    dynamodb_table = "adrai-locking" # this is your dynamoDb table locking name
+  }
 }
 
 provider "aws" {
