a7238d852c6c11cd08762c3e95c1f696642d5c14
Giridhar <giri.ge@mymail.com>
2024-01-30 20:07:44 +0530

    privisoners; lab3 remote-exec 4 nginx install-

--- terraform-provisioners/ec2.tf
+++ terraform-provisioners/ec2.tf
@@ -1,6 +1,7 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "webgiri" {
   ami           = "ami-0f3c7d07486cad139" #devops-practice
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.roboshop-all.id]
 
   tags = {
     Name = "provisioner"
@@ -23,8 +24,57 @@ resource "aws_instance" "web" {
 
  provisioner "local-exec" {
     command = "echo this will execute at the time of destroyyy, you can trigger other system linke email and sending alerts"
-    when = destroy
+    # when = destroy
   } 
 
+connection {
+  type = "ssh"
+  user = "centos"
+  password = "DevOps321"
+  host = self.public_ip
 }
+
+provisioner "remote-exec" {
+    inline = [
+      "echo 'this is from remote exec' > /tmp/remote.txt",
+      "sudo yum install nginx -y",
+      "sudo systemctl start nginx"
+  ] 
+  }
+}
+
+resource "aws_security_group" "roboshop-all" { #this is terraform name, for terraform reference only
+    name        = "provisioner"
+    
+
+    ingress {
+        description      = "Allow All ports"
+        from_port        = 22 # 0 means all ports
+        to_port          = 22 
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    } 
+
+ingress {
+        description      = "Allow All ports"
+        from_port        = 80 # to run nginx from outside you should have opened port number 80
+        to_port          = 80 
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    } 
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        
+    }
+
+    tags = {
+        Name = "Remote-Exec"
+    }
+
+  
+}
+
   
