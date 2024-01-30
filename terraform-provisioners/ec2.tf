68f606a53eb3c147c114f1d357159741bc239c4a
Giridhar <giri.ge@mymail.com>
2024-01-30 14:46:44 +0530

    privisoners; lab 2 whole local-exec codes-

--- terraform-provisioners/ec2.tf
+++ terraform-provisioners/ec2.tf
@@ -5,13 +5,26 @@ resource "aws_instance" "web" {
   tags = {
     Name = "provisioner"
   }
+  provisioner "local-exec" {
+    command = "echo this will execute at the time of creation, you can trigger other system linke email and sending alerts"
+  }
+
 
   provisioner "local-exec" {
     command = "echo the server IP is ${self.private_ip} > inventory"
+    on_failure = fail # it is to stop process if any failure occers
   }
-  }
+  
+
 # provisioner "local-exec" {
 #   command = "ansible-playbook -i inventory web.yaml"
-# }
-  
+# }     # # this code to integrate terraform with ansible but it won’t work here as we don't have an ansible playbook. when we have ansible in linux server this code will work
+
+
+ provisioner "local-exec" {
+    command = "echo this will execute at the time of destroyyy, you can trigger other system linke email and sending alerts"
+    when = destroy
+  } 
+
+}
   
