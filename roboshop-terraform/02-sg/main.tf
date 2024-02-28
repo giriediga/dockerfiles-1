ec569af5961c8da10c9c45299212e7c3f8000488
Giridhar <giri.ge@mymail.com>
2024-02-28 17:40:37 +0530

    creating SG for user with ingress & egress and SG rules for mongodb to accept connection from user instance

--- roboshop-terraform/02-sg/main.tf
+++ roboshop-terraform/02-sg/main.tf
@@ -7,6 +7,138 @@ module "mongodb" {
   sg_description = "SG for MongoDB"
   vpc_id         = data.aws_ssm_parameter.vpc_id.value
   sg_name        = "mongodb"
-  sg_ingress_rules = var.mongodb_sg_ingress_rules
+  # sg_ingress_rules = var.mongodb_sg_ingress_rules
 }
 
+module "catalogue" {
+  source         = "../../terraform-aws-security-group"
+  project_name   = var.project_name
+  environment    = var.environment
+  sg_description = "SG for catalogue"
+  vpc_id         = data.aws_ssm_parameter.vpc_id.value
+  sg_name        = "catalogue"
+  # sg_ingress_rules = var.mongodb_sg_ingress_rules
+}
+
+module "user" {
+  source         = "../../terraform-aws-security-group"
+  project_name   = var.project_name
+  environment    = var.environment
+  sg_description = "SG for user"
+  vpc_id         = data.aws_ssm_parameter.vpc_id.value
+  sg_name        = "user"
+  # sg_ingress_rules = var.mongodb_sg_ingress_rules
+}
+module "cart" {
+  source         = "../../terraform-aws-security-group"
+  project_name   = var.project_name
+  environment    = var.environment
+  sg_description = "SG for cart"
+  vpc_id         = data.aws_ssm_parameter.vpc_id.value
+  sg_name        = "cart"
+  # sg_ingress_rules = var.mongodb_sg_ingress_rules
+}
+module "shipping" {
+  source         = "../../terraform-aws-security-group"
+  project_name   = var.project_name
+  environment    = var.environment
+  sg_description = "SG for shipping"
+  vpc_id         = data.aws_ssm_parameter.vpc_id.value
+  sg_name        = "shipping"
+  # sg_ingress_rules = var.mongodb_sg_ingress_rules
+}
+module "payment" {
+  source         = "../../terraform-aws-security-group"
+  project_name   = var.project_name
+  environment    = var.environment
+  sg_description = "SG for payment"
+  vpc_id         = data.aws_ssm_parameter.vpc_id.value
+  sg_name        = "payment"
+  # sg_ingress_rules = var.mongodb_sg_ingress_rules
+}
+module "web" {
+  source         = "../../terraform-aws-security-group"
+  project_name   = var.project_name
+  environment    = var.environment
+  sg_description = "SG for web"
+  vpc_id         = data.aws_ssm_parameter.vpc_id.value
+  sg_name        = "web"
+  # sg_ingress_rules = var.mongodb_sg_ingress_rules
+}
+
+module "redis" {
+  source         = "../../terraform-aws-security-group"
+  project_name   = var.project_name
+  environment    = var.environment
+  sg_description = "SG for redis"
+  vpc_id         = data.aws_ssm_parameter.vpc_id.value
+  sg_name        = "redis"
+  # sg_ingress_rules = var.mongodb_sg_ingress_rules
+}
+
+module "redis" {
+  source         = "../../terraform-aws-security-group"
+  project_name   = var.project_name
+  environment    = var.environment
+  sg_description = "SG for redis"
+  vpc_id         = data.aws_ssm_parameter.vpc_id.value
+  sg_name        = "redis"
+  # sg_ingress_rules = var.mongodb_sg_ingress_rules
+}
+module "rabbitmq" {
+  source         = "../../terraform-aws-security-group"
+  project_name   = var.project_name
+  environment    = var.environment
+  sg_description = "SG for rabbitmq"
+  vpc_id         = data.aws_ssm_parameter.vpc_id.value
+  sg_name        = "rabbitmq"
+  # sg_ingress_rules = var.mongodb_sg_ingress_rules
+}
+module "mysql" {
+  source         = "../../terraform-aws-security-group"
+  project_name   = var.project_name
+  environment    = var.environment
+  sg_description = "SG for mysql"
+  vpc_id         = data.aws_ssm_parameter.vpc_id.value
+  sg_name        = "mysql"
+  # sg_ingress_rules = var.mongodb_sg_ingress_rules
+}
+
+#SG rule code for mongodb accepting connections from catalogue instance
+resource "aws_security_group_rule" "mongodb_catalogue" {
+  source_security_group_id = module.catalogue.sg_id   # source SG id
+  type                     = "ingress"
+  from_port                = 27017
+  to_port                  = 27017
+  protocol                 = "tcp"
+  security_group_id        = module.mongodb.sg_id # destination SG id
+}
+
+#SG rule code for mongodb accepting connections from user instance
+resource "aws_security_group_rule" "mongodb_user" {
+  source_security_group_id = module.user.sg_id
+  type                     = "ingress"
+  from_port                = 27017
+  to_port                  = 27017
+  protocol                 = "tcp"
+  security_group_id        = module.mongodb.sg_id
+}
+
+#SG rule code for rediss accepting connections from user instance
+resource "aws_security_group_rule" "redis_user" {
+  source_security_group_id = module.user.sg_id    # source SG id
+  type                     = "ingress"
+  from_port                = 6379
+  to_port                  = 6379
+  protocol                 = "tcp"
+  security_group_id        = module.redis.sg_id  # destination SG id
+}
+#SG rule code for rediss accepting connections from cart instance
+resource "aws_security_group_rule" "redis_cart" {
+  source_security_group_id = module.cart.sg_id
+  type                     = "ingress"
+  from_port                = 6379
+  to_port                  = 6379
+  protocol                 = "tcp"
+  security_group_id        = module.redis.sg_id
+}
